ad136a0e74b3200b58ceae90efa0e66e8196b6e2
Antoni Rasul <antoni.rasul@globallogic.com>
2020-01-27 15:16:05 +0100

    Initial commit - containing example of Webflux usage from Spring docs.

---
+++ src/main/java/com/globallogic/reactivestock/ReactiveStockApplication.java
@@ -0,0 +1,13 @@
+package com.globallogic.reactivestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globallogic/reactivestock/ReactiveStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globallogic.reactivestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
