e50773a34053f4f7e0f20e1871572abd4b4be0bf
Antoni Rasul <antoni.rasul@globallogic.com>
2020-01-29 13:56:07 +0100

    Cleaning, fixing mvnw.

--- src/main/java/com/globallogic/reactivestock/ReactiveStockApplication.java
+++ src/main/java/com/globallogic/reactivestock/ReactiveStockApplication.java
@@ -2,14 +2,8 @@ package com.globallogic.reactivestock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories("com.globallogic")
-@EnableMongoAuditing
-@EnableReactiveMongoRepositories
 public class ReactiveStockApplication {
 
 	public static void main(String[] args) {
